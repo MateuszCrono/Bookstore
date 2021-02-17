392a28b95b4ba686879cfe7fc1e84228e2d9c0e3
Mateusz Olszewski <matcrono@gmail.com>
2021-02-17 16:18:36 +0100

    Finished Task 5 ?

--- src/js/script.js
+++ src/js/script.js
@@ -30,7 +30,7 @@ render();
 
 const filters = [];
 const favouriteBooks = [];
-const filteredBook = document.querySelector(select.containerOf.filters)
+const filteredBook = document.querySelector(select.containerOf.filters);
 
 function initActions() {
   const booksContainer = document.querySelector(select.containerOf.booksList);
@@ -55,22 +55,45 @@ function initActions() {
     }
 
   });
-    filteredBook.addEventListener('click', function(event) {
-      event.preventDefault();
-      if (event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter' ) {
-      console.log(event.target.value)
-    }
-      if (event.target.checked == true) {
-        filters.push(event.target.value)
-      }
-      else {
-        const checkedSplice = filters.indexOf(event.target.value)
-        filters.splice(checkedSplice, 1)
-      }
+  filteredBook.addEventListener('click', function(event) {
 
-});
+    if (event.target.tagName == 'INPUT' && event.target.type == 'checkbox' && event.target.name == 'filter' ) {
+      console.log(event.target.value);
+    }
+    if (event.target.checked == true) {
+      filters.push(event.target.value);
+      console.log(filters)
+    }
+    else {
+      const checkedSplice = filters.indexOf(event.target.value);
+      filters.splice(checkedSplice, 1);
+      console.log(filters)
+    }
+
+  });
+  filterBook();
 }
 initActions();
-console.log('favouriteBooks', favouriteBooks);
+
+
+function filterBook() {
+for (let books of dataSource.books) {
+  let shouldbeHidden = false;
+  for (const filter of filters) {
+    if(!books.details[filter]) {
+      shouldbeHidden = true;
+      break;
+    }
+  }
+  if (shouldbeHidden = true ) {
+    document.querySelector('.book__image[data-id=" + books.id + "]').classlist.add('hidden')
+  }
+  else {
+    document.querySelector('.book__image[data-id=" + books.id + "]').classlist.remove('hidden')
+  }
+}
+
+}
+
 
 
